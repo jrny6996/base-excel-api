2d6be63e04f68f1738618c7f36a6ce2c7e07289f
jrny6996 <thejonnyc9@gmail.com>
2025-02-17 09:48:41 -0500

    basic pages and seeded january

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import json
 from flask_cors import CORS
+import sqlite3
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/", methods = ["GET"])
 def index():
-    return "Welcome to the base advanced excel workshop"
+    return render_template("index.html")
 
+@app.route("/guides", methods = ["GET"])
+def guides():
+    return render_template("guides.html")
+
+
+
+# API routes
 @app.route("/api/january", methods = ["POST"])
 def monthly_data():
     if request.json["key"] == 1234:
