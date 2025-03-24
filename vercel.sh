56d3bdd107858db1b863a7205dd975c4430b1f2c
jrny6996 <thejonnyc9@gmail.com>
2025-03-23 23:25:18 -0400

    tailwind vercel

--- vercel.sh
+++ vercel.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 if [[ -f "requirements.txt" ]]; then
   pip install -r requirements.txt
+
+  npm i
+  npx @tailwindcss/cli -i ./static/src/input.css -o ./static/dist/output.css --watch
+
 fi
 
 
